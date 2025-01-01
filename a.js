4ec748c3cac05b9808b3f2318cd862972d8e8123
an12iket <aniketchavan121012@gmail.com>
2025-01-01 22:42:02 +0530

    learned type inference

--- a.js
+++ a.js
@@ -1,3 +1,28 @@
 "use strict";
-const x = 2;
-console.log(x);
+// const x: number = 2;
+// console.log(x);
+// functions in TS
+function greet(firstname) {
+    console.log(`Hello ${firstname}`);
+}
+greet('Aniket');
+function sum(num1, num2) {
+    return num1 + num2;
+}
+const value = sum(1, 3);
+console.log(value);
+function isLegal(age) {
+    if (age >= 18) {
+        return true;
+    }
+    else {
+        return false;
+    }
+}
+isLegal(16);
+function runAfter1s(fn) {
+    setTimeout(fn, 1000);
+}
+runAfter1s(function () {
+    console.log("HELLO");
+});
--- a.ts
+++ a.ts
@@ -1,2 +1,31 @@
-const x: number = 2;
-console.log(x);
+// const x: number = 2;
+// console.log(x);
+
+// functions in TS
+function greet(firstname: String){
+    console.log(`Hello ${firstname}`);
+}
+greet('Aniket');
+
+function sum(num1: number, num2: number): number{
+    return num1 + num2
+}
+const value = sum(1,3)
+console.log(value);
+
+function isLegal(age: number): boolean{
+    if(age >= 18){
+        return true;
+    } else {
+        return false;
+    }
+}
+isLegal(16)
+
+function runAfter1s(fn: () => void){
+    setTimeout(fn, 1000)
+}
+
+runAfter1s(function () {
+    console.log("HELLO");
+})
